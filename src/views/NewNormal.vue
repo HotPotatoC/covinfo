d6197fa8b707b48fe68d06676d62387b8127b23e
HotPotatoC <juandotulung@gmail.com>
2020-06-20 22:28:16 +0700

    feat: updated new normal page

--- src/views/NewNormal.vue
+++ src/views/NewNormal.vue
@@ -4,25 +4,34 @@
       <h1
         class="font-gilroy text-4xl leading-tight md:text-6xl text-primary tracking-tight"
       >New Normal COVID-19:</h1>
-      <p class="font-gilroy text-4xl leading-tight md:text-5xl text-gray-800 tracking-tight">
-        Munculnya kebiasaan - kebiasaan baru di
-        <br />tengah pandemi
-      </p>
-      <img class="mt-4" src="@/assets/images/Frieden-CoV-ThreeSteps-4.6.20-RTS386CQ-SIXTEEN-NINE.jpg" alt />
-      <small
-        class="text-gray-600"
-      >Warga mengambil bagian dalam jam sosialisasi di halaman kompleks apartemen mereka di Pasadena, California pada 5 April 2020 sambil menjaga jarak sosial mereka selama pandemi coronavirus. REUTERS / Mario Anzuoni</small>
+      <p
+        class="font-gilroy text-4xl leading-tight md:text-5xl text-gray-800 tracking-tight"
+      >Munculnya kebiasaan - kebiasaan baru</p>
+      <img
+        class="mt-4"
+        src="@/assets/images/Frieden-CoV-ThreeSteps-4.6.20-RTS386CQ-SIXTEEN-NINE.jpg"
+        alt
+      />
+      <small class="text-gray-600">
+        Warga mengambil bagian dalam jam sosialisasi di halaman kompleks apartemen mereka di Pasadena, California pada 5 April 2020 sambil menjaga jarak sosial mereka selama pandemi coronavirus. REUTERS / Mario Anzuoni
+        <a
+          href="https://www.thinkglobalhealth.org/article/new-normal-covid-19-next-steps-we-must-take"
+          target="__blank"
+        >
+          <sup>[src]</sup>
+        </a>
+      </small>
     </div>
 
     <!-- The New Normal -->
-    <div class="w-full mt-32 max-w-lg">
+    <div class="w-full mt-32 max-w-xl">
       <h1 class="font-gilroy text-5xl tracking-tight" id="the-new-normal">The New Normal</h1>
-      <p class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight">
+      <p class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight">
         <b>New Normal</b> maksudnya apa ya? 🤔
         <br />Disebutkan bahwa New Normal merupakan kebijakan membuka kembali aktifitas ekonomi, sosial dan kegiatan publik secara terbatas. Dengan menggunakan standar atau protokol kesehatan yang ditetapkan oleh Pemerintah.
       </p>
       <br />
-      <p class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight">
+      <p class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight">
         Pada situasi New Normal, tempat kerja perkantoran, industri, tempat kerja sektor jasa dan perdagangan akan dibuka untuk mendukung Keberlangsungan usaha perekonomian.
         <a
           class="text-primary"
@@ -33,26 +42,37 @@
         </a>
       </p>
     </div>
-    <div class="w-full mt-24 flex justify-center">
-      <img src="@/assets/illustrations/undraw_social_distancing_2g0u.svg" alt />
+    <div class="w-full mt-12 flex justify-center">
+      <div class="max-w-4xl">
+        <img src="@/assets/images/Frieden-CoV-ThreeSteps-4.6.20-RTS37VXF-THREE-TWO.jpg" alt />
+        <small class="text-gray-600">
+          Orang-orang menjaga jarak ketika mereka berbelanja selama penguncian yang dilakukan oleh pemerintah di tengah kekhawatiran tentang coronavirus di Kathmandu, Nepal pada 2 April 2020. The REUTERS / Navesh Chitrakar
+          <a
+            href="https://www.thinkglobalhealth.org/article/new-normal-covid-19-next-steps-we-must-take"
+            target="__blank"
+          >
+            <sup>[src]</sup>
+          </a>
+        </small>
+      </div>
     </div>
 
     <!-- New Normal di Indonesia -->
-    <div class="w-full mt-24 max-w-lg">
+    <div class="w-full mt-24 max-w-xl">
       <h1
         class="font-gilroy text-5xl tracking-tight mt-12"
         id="new-normal-di-indonesia"
       >New Normal di Indonesia</h1>
       <p
-        class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight"
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
       >Pada tanggal 15 Mei 2020, istilah New Normal muncul di Indonesia setelah Presiden Joko Widodo menegaskan masyarakat harus bisa berkompromi, hidup berdampingan, dan berdamai dengan Covid-19 agar tetap produktif.</p>
       <br />
       <p
-        class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight"
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
       >Selanjutnya, pada tanggal 20 Mei 2020, Pemerintah pusat melalui Kemenkes mengeluarkan; Kepmenkes No. HK.01.07/Menkes/328/2020 tentang Panduan Pencegahan Dan Covid-19 Di Tempat Kerja Perkantoran Dan Industri Dalam Mendukung Keberlangsungan Usaha Pada Situasi Pandemi</p>
       <br />
-      <p class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight">
-        Surat Edaran Nomor HK.02.01/MENKES/335/2020 Tentang Protokol Pencegahan PenularanCovid-19 Di Tempat Kerja Sektor Jasa Dan Perdagangan (Area Publik) Dalam Mendukung Keberlangsungan Usaha hingga Surat Edaran HK.02.01/MENKES/334/2020 tentang Protokol Pencegahan Penularan Covid-19 bagi Aparat yang Melaksanakan Tugas Pengamanan dan Penertiban Dalam Rangka Percepatan Penanganan Covid-19.
+      <p class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight">
+        Surat Edaran Nomor HK.02.01/MENKES/335/2020 Tentang Protokol Pencegahan Penularan Covid-19 Di Tempat Kerja Sektor Jasa Dan Perdagangan (Area Publik) Dalam Mendukung Keberlangsungan Usaha hingga Surat Edaran HK.02.01/MENKES/334/2020 tentang Protokol Pencegahan Penularan Covid-19 bagi Aparat yang Melaksanakan Tugas Pengamanan dan Penertiban Dalam Rangka Percepatan Penanganan Covid-19.
         <a
           class="text-primary"
           href="https://wahyublahe.id/new-normal/"
@@ -62,19 +82,47 @@
         </a>
       </p>
     </div>
-    <div class="w-full mt-24"></div>
+    <div class="w-full mt-12"></div>
 
     <!-- New Normal dalam Pendidikan -->
-    <div class="w-full mt-24 max-w-lg">
+    <div class="w-full mt-24 max-w-xl">
       <h1
         class="font-gilroy text-5xl tracking-tight mt-12"
         id="new-normal-dalam-pendidikan"
       >New Normal dalam Pendidikan</h1>
       <p
-        class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight"
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
+      >Dengan dimulainya tahun akademik baru di bulan Juli, para pendidik dan pembuat kebijakan harus siap menghadapi norma baru untuk 44 plus juta siswa di seluruh negeri.</p>
+      <img src="@/assets/images/2020_06_12_97539_1591938161._large.jpg" alt />
+      <small class="text-gray-600">
+        Orang-orang menjaga jarak ketika mereka berbelanja selama penguncian yang dilakukan oleh pemerintah di tengah kekhawatiran tentang coronavirus di Kathmandu, Nepal pada 2 April 2020. The REUTERS / Navesh Chitrakar
+        <a
+          href="https://www.thejakartapost.com/academia/2020/06/20/the-new-normal-in-education.html"
+          target="__blank"
+        >
+          <sup>[src]</sup>
+        </a>
+      </small>
+      <br />
+      <br />
+      <p class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight">
+        Sehubungan dengan keputusan bersama menteri oleh menteri pendidikan, menteri urusan agama, menteri dalam negeri dan menteri kesehatan mengumumkan pada tanggal 15 Juni 2020, mengenai pembukaan kembali sekolah selama pandemi, praktik sekolah perlu ditata kembali dan dibentuk kembali untuk mencegah kemungkinan wabah kedua. .
+        Pelajaran yang dipetik selama gangguan sekolah saat ini harus mendorong pendidik untuk mengubah perspektif dan praktik mereka. Membentuk kembali perspektif dan praktik sekolah harus membahas mengapa, siapa, apa, dan bagaimana pendidikan.
+      </p>
+      <br />
+      <p
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
+      >Pandemik saat ini dan kekhawatiran terhadap kemungkinan wabah kedua memang telah mengganggu praktik sekolah tradisional, tetapi cara menciptakan normal baru harus melampaui pandemi saat ini dan menggali lebih dalam daripada takut sakit.</p>
+      <br />
+      <p
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
+      >Penataan ulang bentuk pendidikan baru dapat membuka pintu bagi pendidikan berkualitas yang lebih adil bagi semua anak muda Indonesia. Terlepas dari semua penyakit COVID-19, gangguan pandemi ini telah membawa kesadaran terhadap kemungkinan-kemungkinan baru dalam menghidupkan kembali sistem pendidikan kita dan dalam mengantar anak-anak muda Indonesia ke masa depan dalam bidang permainan yang lebih adil.</p>
+      <br />
+      <p
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
       >Yang pertama, "siapa pendidikan" adalah entitas yang perlu mengubah diri mereka sendiri. Mode belajar-dari-rumah tiba-tiba mengubah peran guru, siswa, dan orang tua. Kebutuhan akan pembelajaran mandiri mengharuskan guru beralih menjadi perancang dan fasilitator pembelajaran alih-alih orang bijak di atas panggung.</p>
       <br />
-      <p class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight">
+      <p class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight">
         Pelajaran yang didapat dari menghilangnya panggung kelas tradisional secara tiba-tiba dan isolasi dari setiap pelajar di ruangnya sendiri harus mendorong para guru untuk melepaskan kebiasaan lama dan memperoleh keterampilan baru dalam keterlibatan pembelajaran online. Berkat gangguan pandemi, pelaksanaan pembelajaran online - tidak peduli seberapa tidak teratur dan tidak merata praktiknya di seluruh negeri - telah memaksa guru untuk menyadari bahwa mereka harus menjangkau setiap siswa secara terpisah dan memeriksa keefektifan pengajaran mereka.
         <a
           class="text-primary"
@@ -85,18 +133,21 @@
         </a>
       </p>
     </div>
-    <div class="w-full mt-24 flex flex justify-center">
-      <img src="@/assets/illustrations/undraw_conference_call_b0w6.svg" alt />
+    <div class="w-full mt-12 flex justify-center">
+      <div class="max-w-4xl">
+        <img src="@/assets/images/Lead_171c035aa84_large.jpg" alt />
+        <small class="text-gray-600">Image Credit: Gulf News Archives</small>
+      </div>
     </div>
 
     <!-- Penerapan New Normal -->
-    <div class="w-full mt-24 max-w-lg">
+    <div class="w-full mt-24 max-w-xl">
       <h1
         class="font-gilroy text-5xl tracking-tight mt-12"
         id="penerapan-new-normal"
       >Penerapan New Normal</h1>
       <p
-        class="font-gilroy font-aileron text-lg text-gray-800 tracking-tight"
+        class="font-gilroy font-aileron text-md md:text-lg text-gray-800 tracking-tight"
       >Protokol pencegahan yang diterapkan pada semua tempat kerja seperti perkantoran, industri, sektor jasa, dan perdagangan dalam implementasi.</p>
       <ul class="list-disc mt-4 ml-4 md:ml-10">
         <li>
@@ -125,7 +176,7 @@
         <sup>[4]</sup>
       </a>
     </div>
-    <div class="w-full mt-24 flex">
+    <div class="w-full mt-12 flex">
       <img src="@/assets/illustrations/undraw_work_from_anywhere_7sdx.svg" alt />
     </div>
   </div>
